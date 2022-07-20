ad0dd9e6fb72083bd441eaa4ac7e268b910fbe7a
denghui <88145482@qq.com>
2022-07-20 18:01:31 +0800

    feat:增加登陆注册系统2

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -2,10 +2,7 @@ import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import React, { useEffect, useState } from "react";
 import { cleanObject, useDebounce, useMount } from "../../utils";
-import * as qs from "qs";
-
-// 定义数据链接
-const apiUrl = process.env.REACT_APP_API_URL;
+import { useHttp } from "../../utils/http";
 
 export const ProjectListScreen = () => {
   // 这个是下拉框的用户选项的状态
@@ -22,25 +19,15 @@ export const ProjectListScreen = () => {
   // 请求结果的列表状态
   const [list, setList] = useState([]);
 
+  const client = useHttp();
+
   // 异步请求数据，当param变化的时候
   useEffect(() => {
-    // 这里使用qs对对象进行参数化解析 会将对象解析成 key=value&key=value
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`
-    ).then(async (respone) => {
-      // 当请求数据返回ok的状态的时候
-      if (respone.ok) {
-        setList(await respone.json());
-      }
-    });
+    client("projects", { data: cleanObject(debounceParam) }).then(setList);
   }, [debounceParam]);
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (respone) => {
-      if (respone.ok) {
-        setUsers(await respone.json());
-      }
-    });
+    client("users").then(setUsers);
   });
 
   return (

055c8fa3b3d3a0ceb90ec6c8dc114b81807be243
denghui <88145482@qq.com>
2022-07-21 20:07:57 +0800

    style:安装了emotion，优化登录注册界面，增加了图片样式声明文件

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,12 +2,26 @@
 import React from "react";
 import { ProjectListScreen } from "./screens/project-list";
 import { useAuth } from "./context/auth-context";
+import styled from "@emotion/styled";
+
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <div>
+      <PageHeader />
       <button onClick={logout}>登出</button>
-      <ProjectListScreen />
+      <Main>
+        <ProjectListScreen />
+      </Main>
     </div>
   );
 };
+
+const PageHeader = styled.header`
+  background-color: gray;
+  height: 6rem;
+`;
+
+const Main = styled.main`
+  height: calc(100vh - 6rem);
+`;

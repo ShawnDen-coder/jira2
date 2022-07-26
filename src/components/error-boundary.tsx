a2df31c087e16d95a12c2bdc0ef30dad6642d3cd
denghui <88145482@qq.com>
2022-07-26 18:11:03 +0800

    feat:添加边界异常处理

---
+++ src/components/error-boundary.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+// 错误边界的实现一定需要使用 class 组件去实现
+type FallbackRender = (props: { error: Error | null }) => React.ReactElement;
+
+export class ErrorBoundary extends React.Component<
+  React.PropsWithChildren<{ fallbackRender: FallbackRender }>,
+  { error: Error | null }
+> {
+  state = { error: null };
+
+  // 当子组件抛出异常，这里会接收到并调用
+  static getDerivedStateFromError(error: Error) {
+    return { error };
+  }
+
+  render() {
+    const { error } = this.state;
+    const { fallbackRender, children } = this.props;
+
+    if (error) {
+      return fallbackRender({ error });
+    }
+    return children;
+  }
+}
